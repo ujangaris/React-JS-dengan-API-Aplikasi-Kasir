164a7b95eb27425638dbf5218edbcd451188b023
ujangaja <ujangaja@gmail.com>
2021-07-19 21:31:06 +0700

    Tutorial React JS dengan API #5. Menampilkan Menu Produk | menampilkan gambar

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,17 +1,23 @@
 // rafce
 import React from "react";
-import { Col,Card } from "react-bootstrap";
-import {numberWithCommas} from '../utils/utils';
+import { Col, Card } from "react-bootstrap";
+import { numberWithCommas } from "../utils/utils";
 const Menus = ({ menu }) => {
   return (
     <Col md={4} xs={6} className="mt-4">
-      <Card >
-        <Card.Img variant="top" src="holder.js/100px180" />
+      <Card className="shadow">
+        <Card.Img
+          variant="top"
+          src={
+            "assets/images/" +
+            menu.category.nama.toLowerCase() +
+            "/" +
+            menu.gambar
+          }
+        />
         <Card.Body>
-          <Card.Title>{menu.nama}</Card.Title>
-          <Card.Text>
-            Rp. {numberWithCommas(menu.harga)}
-          </Card.Text>
+          <Card.Title>{menu.nama} <strong> ({menu.kode})</strong></Card.Title>
+          <Card.Text>Rp. {numberWithCommas(menu.harga)}</Card.Text>
         </Card.Body>
       </Card>
     </Col>
