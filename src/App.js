36a039076603639b2a70adb9acb7fd7f6f56cb70
ujangaja <ujangaja@gmail.com>
2021-07-19 20:47:57 +0700

    Tutorial React JS dengan API #5. Menampilkan Menu Produk | menampilkan data kedalam console

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 import { Hasil, ListCategories, NavbarComponent } from "./components";
 import { Row, Col, Container } from "react-bootstrap";
-//rrc
+import { API_URL } from "./utils/constants";
+import axios from "axios";
 
+//rrc
 export default class App extends Component {
+  //rconst
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "product")
+      .then((res) => {
+        console.log("Response : ", res);
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log("Error ya: ",error);
+      });
+  }
+
   render() {
+    console.log(this.state.menus);
     return (
       <div className="App">
         <NavbarComponent />
