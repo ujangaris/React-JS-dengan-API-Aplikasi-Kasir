904d889fe63130c70cb3dc206263124df0957cce
ujangaja <ujangaja@gmail.com>
2021-07-23 20:15:48 +0700

    React JS dengan API : 6. Menampilkan Produk berdasarkan Category | menampilkan pada console log

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -1,14 +1,41 @@
 // rcc
-import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Col } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
 
 export default class ListCategories extends Component {
-    render() {
-        return (
-            <Col md={2} mt="2">
-                <h4><strong>Daftar Kategori</strong></h4>
-                <hr />
-            </Col>
-        )
-    }
+  //rcons
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "categories")
+      .then((res) => {
+        // console.log("Response : ", res);
+        const categories = res.data;
+        this.setState({ categories });
+      })
+      .catch((error) => {
+        console.log("Error ya: ", error);
+      });
+  }
+
+  render() {
+      console.log("Catgeories ", this.state.categories);
+    return (
+      <Col md={2} mt="2">
+        <h4>
+          <strong>Daftar Kategori</strong>
+        </h4>
+        <hr />
+      </Col>
+    );
+  }
 }
