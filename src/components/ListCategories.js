c8d258ce58affcab04a73277841c0b4b3ee1c1a3
ujangaja <ujangaja@gmail.com>
2021-07-23 20:37:32 +0700

    React JS dengan API : 6. Menampilkan Produk berdasarkan Category | menampilkan Icon pada web/html

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -3,7 +3,23 @@ import React, { Component } from "react";
 import { Col, ListGroup } from "react-bootstrap";
 import axios from "axios";
 import { API_URL } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faCoffee,
+  faCheese,
+} from "@fortawesome/free-solid-svg-icons";
 
+const Icon = ({ nama }) => {
+  if (nama === "Makanan")
+    return <FontAwesomeIcon icon={faUtensils} className="mr-2" />;
+  if (nama === "Minuman")
+    return <FontAwesomeIcon icon={faCoffee}  />;
+  if (nama === "Cemilan")
+    return <FontAwesomeIcon icon={faCheese} className="mr-2" />;
+
+  return <FontAwesomeIcon icon={faUtensils} className="mr-2" />;
+};
 export default class ListCategories extends Component {
   //rcons
   constructor(props) {
@@ -40,7 +56,11 @@ export default class ListCategories extends Component {
         <ListGroup>
           {categories &&
             categories.map((category) => (
-              <ListGroup.Item key={category.id}><h5>{category.nama}</h5></ListGroup.Item>
+              <ListGroup.Item key={category.id}>
+                <h5>
+                   <Icon nama={category.nama }/> {category.nama}
+                </h5>
+              </ListGroup.Item>
             ))}
         </ListGroup>
       </Col>
