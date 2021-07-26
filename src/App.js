7ba67790129a634b52a29870a6c73fd5c3ba05ba
ujangaja <ujangaja@gmail.com>
2021-07-26 21:13:51 +0700

    Tutorial React JS dengan API : #8 Setting React Router DOM | setting pages

--- src/App.js
+++ src/App.js
@@ -1,171 +1,24 @@
-import React, { Component } from "react";
-import { Hasil, ListCategories, NavbarComponent, Menus } from "./components";
-import { Row, Col, Container } from "react-bootstrap";
-import { API_URL } from "./utils/constants";
-import axios from "axios";
-import swal from "sweetalert";
+import React, { Component } from 'react'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { NavbarComponent } from './components';
+import {Home, Sukses} from './pages';
 
-//rrc
 export default class App extends Component {
-  //rconst
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      menus: [],
-      categoriYangDipilih: "Makanan",
-      keranjangs: [],
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get(API_URL + "products?category.nama=" + this.state.categoriYangDipilih)
-      .then((res) => {
-        // console.log("Response : ", res);
-        const menus = res.data;
-        this.setState({ menus });
-      })
-      .catch((error) => {
-        console.log("Error ya: ", error);
-      });
-
-    //memasukan kedalam hasil
-    axios
-      .get(API_URL + "keranjangs")
-      .then((res) => {
-        // console.log("Response : ", res);
-        const keranjangs = res.data;
-        this.setState({ keranjangs });
-      })
-      .catch((error) => {
-        console.log("Error ya: ", error);
-      });
-  }
-
-  componentDidUpdate(prevState) {
-    if (this.state.keranjangs !== prevState.keranjangs) {
-      //menjalakan/memasukan kedalam hasil
-      axios
-        .get(API_URL + "keranjangs")
-        .then((res) => {
-          // console.log("Response : ", res);
-          const keranjangs = res.data;
-          this.setState({ keranjangs });
-        })
-        .catch((error) => {
-          console.log("Error ya: ", error);
-        });
-    }
-  }
-
-  changeCategory = (value) => {
-    this.setState({
-      categoriYangDipilih: value,
-      menus: [],
-    });
-
-    axios
-      .get(API_URL + "products?category.nama=" + value)
-      .then((res) => {
-        // console.log("Response : ", res);
-        const menus = res.data;
-        this.setState({ menus });
-      })
-      .catch((error) => {
-        console.log("Error ya: ", error);
-      });
-  };
-
-  masukKeranjang = (value) => {
-    // console.log("Menu : ", value);
-    axios
-      .get(API_URL + "keranjangs?product.id=" + value.id)
-      .then((res) => {
-        if (res.data.length === 0) {
-          const keranjang = {
-            jumlah: 1,
-            total_harga: value.harga,
-            product: value,
-          };
-
-          axios
-            .post(API_URL + "keranjangs", keranjang)
-            .then((res) => {
-              swal({
-                title: "Sukses Masuk Keranjang!",
-                text: "Sukses Masuk Keranjang " + keranjang.product.nama,
-                icon: "success",
-                button: false,
-                timer: 1500,
-              });
-            })
-            .catch((error) => {
-              console.log("Error ya: ", error);
-            });
-        } else {
-          const keranjang = {
-            jumlah: res.data[0].jumlah + 1,
-            total_harga: res.data[0].total_harga + value.harga,
-            product: value,
-          };
-
-          //jika berhasil
-          axios
-            .put(API_URL + "keranjangs/" + res.data[0].id, keranjang)
-            .then((res) => {
-              swal({
-                title: "Sukses Masuk Keranjang!",
-                text: "Sukses Masuk Keranjang " + keranjang.product.nama,
-                icon: "success",
-                button: false,
-                timer: 1500,
-              });
-            })
-            .catch((error) => {
-              console.log("Error ya: ", error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.log("Error ya: ", error);
-      });
-  };
-
   render() {
-    // console.log(this.state.menus);
-    const { menus, categoriYangDipilih, keranjangs } = this.state;
     return (
-      <div className="App">
+      <BrowserRouter>
         <NavbarComponent />
-        <div className="mt-3">
-          <Container fluid>
-            <Row>
-              <ListCategories
-                changeCategory={this.changeCategory}
-                categoriYangDipilih={categoriYangDipilih}
-              />
-              <Col>
-                <h4>
-                  <strong>Daftar Produk</strong>
-                </h4>
-                <hr />
-                <Row>
-                  {menus &&
-                    menus.map((menu) => (
-                      <Menus
-                        key={menu.id}
-                        menu={menu}
-                        masukKeranjang={this.masukKeranjang}
-                      />
-                    ))}
-                </Row>
-              </Col>
-              <Hasil keranjangs={keranjangs} />
-            </Row>
-          </Container>
-        </div>
-      </div>
-    );
+          <main>
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/sukses" component={Sukses} exact />
+            </Switch>
+          </main>
+      </BrowserRouter>
+    )
   }
 }
---
+++ src/pages/Sukses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Sukses extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Sukses</h2>
+            </div>
+        )
+    }
+}
