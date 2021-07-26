34c7ef201c271bd0756473d26032fc63dfc35dce
ujangaja <ujangaja@gmail.com>
2021-07-26 21:30:50 +0700

    Tutorial React JS dengan API : #8 Setting React Router DOM | Halaman Sukses

--- src/pages/Sukses.js
+++ src/pages/Sukses.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import { Button, Image } from 'react-bootstrap'
+import {Link} from 'react-router-dom';
 
 export default class Sukses extends Component {
     render() {
         return (
-            <div>
-                <h2>Sukses</h2>
+            <div className="mt-4 text-center">
+                <Image src="assets/images/sukses.png" width="500"/>
+                <h2>Sukses Pesan</h2>
+                <p>Terimakasi Sudah Memesan!</p>
+                <Button variant="primary" as={Link} to="/">
+                    Kembali
+                </Button>
             </div>
         )
     }
