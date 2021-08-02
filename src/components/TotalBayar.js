27dcd7b3156934e96a918f09e55ef25b5020f9cd
ujangaja <ujangaja@gmail.com>
2021-08-02 20:24:52 +0700

    Tutorial React JS dengan API : #9. Menghitung Total Bayar | menambahkan Icon pada buttom bayar

--- src/components/TotalBayar.js
+++ src/components/TotalBayar.js
@@ -2,6 +2,8 @@
 import React, { Component } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 export default class TotalBayar extends Component {
   render() {
@@ -19,13 +21,10 @@ export default class TotalBayar extends Component {
               </strong>
             </h4>
             <div className="d-grid">
-
-            <Button
-              variant="primary"
-              block >
-              <strong>BAYAR</strong>
-            </Button>
-                </div>
+              <Button variant="primary" block>
+               <FontAwesomeIcon icon={faShoppingCart}/> <strong>BAYAR</strong>
+              </Button>
+            </div>
           </Col>
         </Row>
       </div>
