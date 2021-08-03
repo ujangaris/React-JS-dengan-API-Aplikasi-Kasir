fe91201b41baa5961c09f4f0e9d3a11a55fadfa7
ujangaja <ujangaja@gmail.com>
2021-08-03 20:31:47 +0700

    Tutorial React JS dengan API : #10. Membuat Modal Edit Pemesanan | menampilkan modal keranjang detail (nama dan harga)

--- src/components/ModalKeranjang.js
+++ src/components/ModalKeranjang.js
@@ -1,24 +1,51 @@
 //rafce
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import { numberWithCommas } from './../utils/utils';
 
-const ModalKeranjang = ({ showModal, handleClose }) => {
-  return (
-    <Modal show={showModal} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+const ModalKeranjang = ({ showModal, handleClose, keranjangDetail }) => {
+  if (keranjangDetail) {
+    return (
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+            <Modal.Title>
+              {keranjangDetail.product.nama}{" "}
+              <strong>
+                  (Rp. {numberWithCommas(keranjangDetail.product.harga)})
+              </strong>
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            Woohoo, you're reading this text in a modal!
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  } else {
+    return (
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Kosong</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Kosong</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 };
 
 export default ModalKeranjang;
