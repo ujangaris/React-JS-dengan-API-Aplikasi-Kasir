ccf143b217a3eea7bcef4555d386ddf2afc85a67
ujangaja <ujangaja@gmail.com>
2021-08-09 20:28:37 +0700

    Tutorial React JS dengan API : #10. Membuat Modal Edit Pemesanan | Form edit pesanan

--- src/components/ModalKeranjang.js
+++ src/components/ModalKeranjang.js
@@ -1,29 +1,70 @@
 //rafce
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
-import { numberWithCommas } from './../utils/utils';
+import { Modal, Button, Form } from "react-bootstrap";
+import { numberWithCommas } from "./../utils/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const ModalKeranjang = ({ showModal, handleClose, keranjangDetail }) => {
+const ModalKeranjang = ({
+  showModal,
+  handleClose,
+  keranjangDetail,
+  jumlah,
+  keterangan,
+}) => {
   if (keranjangDetail) {
     return (
       <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
-            <Modal.Title>
-              {keranjangDetail.product.nama}{" "}
-              <strong>
-                  (Rp. {numberWithCommas(keranjangDetail.product.harga)})
-              </strong>
-            </Modal.Title>
+          <Modal.Title>
+            {keranjangDetail.product.nama}{" "}
+            <strong>
+              (Rp. {numberWithCommas(keranjangDetail.product.harga)})
+            </strong>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            Woohoo, you're reading this text in a modal!
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Total Harga :</Form.Label>
+              <strong>
+                <p>Rp. {numberWithCommas(keranjangDetail.total_harga)}</p>
+              </strong>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Jumlah :</Form.Label>
+              <br />
+              <Button variant="primary" size="sm" className="mr-2">
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>{" "}
+              <strong>{jumlah}</strong>{" "}
+              <Button variant="primary" size="sm" className="ml-2">
+                <FontAwesomeIcon icon={faMinus} />
+              </Button>
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Keterangan :</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                name="keterangan"
+                placeholder="Contoh : Pedas, Nasi 1/2"
+                value={keterangan}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Simpan
+            </Button>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="danger" onClick={handleClose}>
+            <FontAwesomeIcon icon={faTrash} /> Hapus Pesanan
           </Button>
         </Modal.Footer>
       </Modal>
