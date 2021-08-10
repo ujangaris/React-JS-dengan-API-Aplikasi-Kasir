3f7d891beb9f0f752337147c81dc5da830b53c6b
ujangaja <ujangaja@gmail.com>
2021-08-10 21:03:51 +0700

    Tutorial React JS dengan API : #12. Membuat Website Lebih Responsif | responsive scrol untuk keranjang

--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -1,6 +1,6 @@
 // rcc
 import React, { Component } from "react";
-import { Badge, Col, ListGroup, Row } from "react-bootstrap";
+import { Badge, CardColumns, Col, ListGroup, Row, Card } from "react-bootstrap";
 import { numberWithCommas } from "./../utils/utils";
 import ModalKeranjang from "./ModalKeranjang";
 import TotalBayar from "./TotalBayar";
@@ -118,43 +118,45 @@ export default class ListCategories extends Component {
         </h4>
         <hr />
         {keranjangs.length !== 0 && (
-          <ListGroup variant="flush">
-            {keranjangs.map((menuKeranjang) => (
-              <ListGroup.Item
-                key={menuKeranjang.id}
-                onClick={() => this.handleShow(menuKeranjang)}
-              >
-                <Row>
-                  <Col xs={2}>
-                    <h4>
-                      <Badge pill bg="success">
-                        {menuKeranjang.jumlah}
-                      </Badge>
-                    </h4>
-                  </Col>
-                  <Col>
-                    <h5>{menuKeranjang.product.nama}</h5>
-                    <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
-                  </Col>
-                  <Col>
-                    <strong className="float-right">
-                      Rp. {numberWithCommas(menuKeranjang.total_harga)}
-                    </strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            ))}
+          <Card className="overflow-auto hasil">
+            <ListGroup variant="flush">
+              {keranjangs.map((menuKeranjang) => (
+                <ListGroup.Item
+                  key={menuKeranjang.id}
+                  onClick={() => this.handleShow(menuKeranjang)}
+                >
+                  <Row>
+                    <Col xs={2}>
+                      <h4>
+                        <Badge pill bg="success">
+                          {menuKeranjang.jumlah}
+                        </Badge>
+                      </h4>
+                    </Col>
+                    <Col>
+                      <h5>{menuKeranjang.product.nama}</h5>
+                      <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
+                    </Col>
+                    <Col>
+                      <strong className="float-right">
+                        Rp. {numberWithCommas(menuKeranjang.total_harga)}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
 
-            <ModalKeranjang
-              handleClose={this.handleClose}
-              {...this.state}
-              tambah={this.tambah}
-              kurang={this.kurang}
-              changeHandler={this.changeHandler}
-              handleSubmit={this.handleSubmit}
-              hapusPesanan={this.hapusPesanan}
-            />
-          </ListGroup>
+              <ModalKeranjang
+                handleClose={this.handleClose}
+                {...this.state}
+                tambah={this.tambah}
+                kurang={this.kurang}
+                changeHandler={this.changeHandler}
+                handleSubmit={this.handleSubmit}
+                hapusPesanan={this.hapusPesanan}
+              />
+            </ListGroup>
+          </Card>
         )}
         <TotalBayar keranjangs={keranjangs} {...this.props} />
       </Col>
